d131e99714f22ea183d82715185c8153a57f4e07
Saurabh Singh <Saurabhsingh1138112@gmail.com>
2025-07-11 03:31:31 +0530

    created header component

--- src/App.tsx
+++ src/App.tsx
@@ -10,33 +10,40 @@ import { SignInPage } from "@/routes/sign-in";
 import { SignUpPage } from "@/routes/sign-up";
 import ProtectedRoutes from "@/layouts/protected-routes";
 
-
+// Dummy protected page
+const DashboardPage = () => {
+  return <h1 className="text-3xl font-bold">Dashboard Page</h1>;
+};
 
 const App = () => {
   return (
-   <Router>
-    <Routes>
-      {/* public routes */}
-       <Route element={<PublicLayout />}>
-       <Route index element={<HomePage />} />
+    <Router>
+      <Routes>
+        {/* public routes */}
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
         </Route>
 
         {/* authentication layout */}
-       <Route element={<AuthenticationLayout />}>
-       <Route path="/signin/*" element={<SignInPage />} />
-       <Route path="/signup/*" element={<SignUpPage />} />
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
         </Route>
 
-      {/* protected routes */}
-      <Route element={<ProtectedRoutes><MainLayout /></ProtectedRoutes>
-    }
-    >
-      {/* add all the protected Routes */}
-
-      </Route>
-    </Routes>
-   </Router>
+        {/* protected routes */}
+        <Route
+          element={
+            <ProtectedRoutes>
+              <MainLayout />
+            </ProtectedRoutes>
+          }
+        >
+          <Route path="/dashboard" element={<DashboardPage />} />
+          {/* Baad me yaha aur protected pages add kar sakta */}
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
-export default App
+export default App;
