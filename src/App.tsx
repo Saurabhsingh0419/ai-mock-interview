f1a45f179811930ab18c955ba50f8b266bdf45fb
Saurabh Singh <Saurabhsingh1138112@gmail.com>
2025-07-11 01:07:54 +0530

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { PublicLayout } from "@/layouts/public-layout";
 import HomePage from "@/routes/home";
+import { MainLayout } from "@/layouts/main-layout";
 
 import AuthenticationLayout from "@/layouts/auth-layout";
 
 import { SignInPage } from "@/routes/sign-in";
 import { SignUpPage } from "@/routes/sign-up";
+import ProtectedRoutes from "@/layouts/protected-routes";
 
 
 
@@ -26,6 +28,12 @@ const App = () => {
         </Route>
 
       {/* protected routes */}
+      <Route element={<ProtectedRoutes><MainLayout /></ProtectedRoutes>
+    }
+    >
+      {/* add all the protected Routes */}
+
+      </Route>
     </Routes>
    </Router>
   );
